58a460c59a61b06e8ba486a36f2bfaefa6092fc2
Mikalai Budai <kolyabudai@gmail.com>
2024-12-29 23:27:24 +0100

    minor change

--- Sources/MyNetworkService/NetworkClient.swift
+++ Sources/MyNetworkService/NetworkClient.swift
@@ -58,59 +58,57 @@ public extension NetworkClient {
         }
     }
     
-    extension NetworkClient {
-        func sendPostRequest<T: Decodable, U: Encodable>(
-            endpoint: Endpoint,
-            requestBody: U,
-            responseModel: T.Type? = nil
-        ) async -> Result<T?, RequestError> {
-            var urlComponents = URLComponents()
-            urlComponents.scheme = endpoint.scheme
-            urlComponents.host = endpoint.host
-            urlComponents.path = endpoint.path
-            urlComponents.queryItems = endpoint.queryItems
+    func sendPostRequest<T: Decodable, U: Encodable>(
+        endpoint: Endpoint,
+        requestBody: U,
+        responseModel: T.Type? = nil
+    ) async -> Result<T?, RequestError> {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = endpoint.scheme
+        urlComponents.host = endpoint.host
+        urlComponents.path = endpoint.path
+        urlComponents.queryItems = endpoint.queryItems
 
-            guard let url = urlComponents.url else {
-                return .failure(.invalidURL)
+        guard let url = urlComponents.url else {
+            return .failure(.invalidURL)
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = endpoint.header
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        do {
+            request.httpBody = try JSONEncoder().encode(requestBody)
+        } catch {
+            return .failure(.encoding)
+        }
+
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request, delegate: nil)
+            guard let httpResponse = response as? HTTPURLResponse else {
+                return .failure(.noResponse)
             }
 
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST"
-            request.allHTTPHeaderFields = endpoint.header
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-
-            do {
-                request.httpBody = try JSONEncoder().encode(requestBody)
-            } catch {
-                return .failure(.encoding)
-            }
-
-            do {
-                let (data, response) = try await URLSession.shared.data(for: request, delegate: nil)
-                guard let httpResponse = response as? HTTPURLResponse else {
-                    return .failure(.noResponse)
-                }
-
-                switch httpResponse.statusCode {
-                case 200...299:
-                    if let responseModel = responseModel {
-                        do {
-                            let decodedResponse = try JSONDecoder().decode(responseModel, from: data)
-                            return .success(decodedResponse)
-                        } catch {
-                            return .failure(.decoding)
-                        }
-                    } else {
-                        return .success(nil)
+            switch httpResponse.statusCode {
+            case 200...299:
+                if let responseModel = responseModel {
+                    do {
+                        let decodedResponse = try JSONDecoder().decode(responseModel, from: data)
+                        return .success(decodedResponse)
+                    } catch {
+                        return .failure(.decoding)
                     }
-                case 401:
-                    return .failure(.unauthorized)
-                default:
-                    return .failure(.unexpectedStatusCode)
+                } else {
+                    return .success(nil)
                 }
-            } catch {
-                return .failure(.unknown)
+            case 401:
+                return .failure(.unauthorized)
+            default:
+                return .failure(.unexpectedStatusCode)
             }
+        } catch {
+            return .failure(.unknown)
         }
     }
 }
