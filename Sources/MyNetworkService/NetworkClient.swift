a406b5c9c5469a570f62cddf9295043446a88946
Mikalai Budai <kolyabudai@gmail.com>
2024-12-29 22:47:06 +0100

    add post request

--- Sources/MyNetworkService/NetworkClient.swift
+++ Sources/MyNetworkService/NetworkClient.swift
@@ -9,6 +9,7 @@ import Foundation
 
 public protocol NetworkClient {
     func sendRequest<T: Decodable>(endpoint: Endpoint, responseModel: T.Type) async -> Result<T, RequestError>
+    func sendPostRequest<T: Decodable, U: Encodable>(endpoint: Endpoint, postModel: U, responseModel: T.Type? = nil) async -> Result<T?, RequestError>
 }
 
 @available(iOS 15.0, *)
@@ -56,4 +57,51 @@ public extension NetworkClient {
             return .failure(.unknown)
         }
     }
+    
+    func sendPostRequest<T: Decodable, U: Encodable>(
+            endpoint: Endpoint,
+            postModel: U,
+            responseModel: T.Type? = nil
+        ) async -> Result<T?, RequestError> {
+            var urlComponents = URLComponents()
+            urlComponents.scheme = endpoint.scheme
+            urlComponents.host = endpoint.host
+            urlComponents.path = endpoint.path
+            urlComponents.queryItems = endpoint.queryItems
+
+            guard let url = urlComponents.url else {
+                return .failure(.invalidURL)
+            }
+
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.allHTTPHeaderFields = endpoint.header
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+            do {
+                request.httpBody = try JSONEncoder().encode(requestBody)
+            } catch {
+                return .failure(.encoding)
+            }
+
+            do {
+                let (data, response) = try await URLSession.shared.data(for: request, delegate: nil)
+                guard let response = response as? HTTPURLResponse else {
+                    return .failure(.noResponse)
+                }
+                switch response.statusCode {
+                case 200...299:
+                    guard let decodedResponse = try? JSONDecoder().decode(responseModel, from: data) else {
+                        return .failure(.decoding)
+                    }
+                    return .success(decodedResponse)
+                case 401:
+                    return .failure(.unauthorized)
+                default:
+                    return .failure(.unexpectedStatusCode)
+                }
+            } catch {
+                return .failure(.unknown)
+            }
+        }
 }
